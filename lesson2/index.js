a62916a620f4a131cd6664453aa6303b87f43fc0
Catharin <jekaterina.shashkina@gmail.com>
2023-04-26 22:08:00 +0300

    second lesson

--- lesson2/index.js
+++ lesson2/index.js
@@ -1,1 +1,25 @@
 "use strict";
+
+{
+  const product = "IPhone 12";
+
+  const count = 10;
+
+  const productCategory = "Mobile";
+
+  const productPrice = 1200;
+
+  console.log(product);
+
+  console.log(count * productPrice);
+}
+
+{
+  const product = "IPad";
+  const count = 7;
+  const productCategory = "Tablet";
+  const productPrice = 800;
+  console.log(product);
+
+  console.log(count * productPrice);
+}
