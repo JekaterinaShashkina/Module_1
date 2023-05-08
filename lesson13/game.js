09f4c50f7bad577286f6b1b062804a164bf2233d
Catharin <jekaterina.shashkina@gmail.com>
2023-05-08 20:59:05 +0300

    main task

--- lesson13/game.js
+++ lesson13/game.js
@@ -21,11 +21,16 @@
       language === 'EN' || language === 'ENG' ? FIGURES_ENG : FIGURES_RUS;
     return function start() {
       // сюда писать логику игры
-      const choices = () => {
       let userChoice = prompt('Камень, ножницы, бумага?');
       const computerChoiceNumber = getRandomIntInclusive(0, 2);
       let computerChoice = '';
-      console.log(computerChoice);
+      if (userChoice === null) {
+        let end = confirm('Хотите закончить?');
+        alert(
+          `Результат игрока: ${result.player}\nРезультат компьютера ${result.computer}`,
+        );
+        return end;
+      }
       for (let i = 0; i < FIGURES_RUS.length; i++) {
         if (FIGURES_RUS[i].startsWith(userChoice.toLowerCase())) {
           userChoice = FIGURES_RUS[i];
@@ -33,55 +38,28 @@
         if (i === computerChoiceNumber) {
           computerChoice = FIGURES_RUS[i];
         }
-      console.log(userChoice);
-      console.log(`${userChoice[0]} ${computerChoice[0]}`);
-        return {userChoice, computerChoice};
       }
-    }
-    let u = '';
-    let c = '';
-    u, c = choices()
-    console.log("new words: ", u, c);
 
-
-      const checkResult = (u, c) => {
-        if (u === null) {
-          return confirm('Хотите закончить?');
-        }
-        if (c === u) {
-          alert("it's a draw");
-          u, c = choices()
-        } else if (
-          (c === FIGURES_RUS[0] && u === FIGURES_RUS[2]) ||
-          (c === FIGURES_RUS[2] && u === FIGURES_RUS[1]) ||
-          (c === FIGURES_RUS[1] && u === FIGURES_RUS[0])
-        ) {
-          alert(`Вы выиграли\nВаш выбор: ${u}\nВыбор компьютера: ${c}`);
-          u, c = choices()
-        } else {
-          alert(`Вы проиграли\nВаш выбор: ${u}\nВыбор компьютера: ${c}`);
-          u, c = choices()
-
-        }
-        checkResult(u, c);
-      };
-      checkResult(choices());
-
-      // if (computerChoice === userChoice) {
-      //   alert("it's a draw");
-      // } else if (
-      //   (computerChoice === FIGURES_RUS[0] && userChoice === FIGURES_RUS[2]) ||
-      //   (computerChoice === FIGURES_RUS[2] && userChoice === FIGURES_RUS[1]) ||
-      //   (computerChoice === FIGURES_RUS[1] && userChoice === FIGURES_RUS[0])
-      // ) {
-      //   alert(
-      //     `Вы выиграли\nВаш выбор: ${userChoice}\nВыбор компьютера: ${computerChoice}`,
-      //   );
-      // } else {
-      //   alert(
-      //     `Вы проиграли\nВаш выбор: ${userChoice}\nВыбор компьютера: ${computerChoice}`,
-      //   );
-      // }
+      if (computerChoice === userChoice) {
+        alert(
+          `Ничья\nВаш выбор: ${userChoice}\nВыбор компьютера: ${computerChoice}`,
+        );
+      } else if (
+        (computerChoice === FIGURES_RUS[0] && userChoice === FIGURES_RUS[2]) ||
+        (computerChoice === FIGURES_RUS[2] && userChoice === FIGURES_RUS[1]) ||
+        (computerChoice === FIGURES_RUS[1] && userChoice === FIGURES_RUS[0])
+      ) {
+        alert(
+          `Вы выиграли\nВаш выбор: ${userChoice}\nВыбор компьютера: ${computerChoice}`,
+        );
+        result.player += 1;
+      } else {
+        alert(
+          `Вы проиграли\nВаш выбор: ${userChoice}\nВыбор компьютера: ${computerChoice}`,
+        );
+        result.computer += 1;
+      }
+      start();
     };
   };
   window.RPS = game;
