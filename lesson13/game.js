533de02ab16c0fde50001e268a470a162d967db0
Catharin <jekaterina.shashkina@gmail.com>
2023-05-12 18:34:30 +0300

    last changes

--- lesson13/game.js
+++ lesson13/game.js
@@ -11,28 +11,27 @@
   };
 
   const getFigure = (lang) => {
-    const userQuestion = []
-    if (lang == FIGURES_ENG) {
-      userQuestion.push('Rock, scissors, paper?')
-      userQuestion.push('Want finish?')
-      userQuestion.push('Players result: ')
-      userQuestion.push('\nComputer result: ')
-      userQuestion.push('Its draw\nUser choice: ')
-      userQuestion.push('You win\nUser choice: ')
-      userQuestion.push('You lose\nUser choice: ')
-      userQuestion.push('\nComputer choice: ')
+    const userQuestion = [];
+    if (lang === FIGURES_ENG) {
+      userQuestion.push('Rock, scissors, paper?');
+      userQuestion.push('Want finish?');
+      userQuestion.push('Players result: ');
+      userQuestion.push('\nComputer result: ');
+      userQuestion.push('Its draw\nUser choice: ');
+      userQuestion.push('You win\nUser choice: ');
+      userQuestion.push('You lose\nUser choice: ');
+      userQuestion.push('\nComputer choice: ');
     } else {
-      userQuestion.push('Камень, ножницы, бумага?')
-      userQuestion.push('Хотите закончить?')
-      userQuestion.push('Результат игрока: ')
-      userQuestion.push('\nРезультат компьютера: ')
-      userQuestion.push('Ничья\nВыбор игрока: ')
-      userQuestion.push('Вы выиграли\nВыбор игрока: ')
-      userQuestion.push('Вы проиграли\nВыбор игрока: ')
-      userQuestion.push('\nВыбор компьютера: ')
-
+      userQuestion.push('Камень, ножницы, бумага?');
+      userQuestion.push('Хотите закончить?');
+      userQuestion.push('Результат игрока: ');
+      userQuestion.push('\nРезультат компьютера: ');
+      userQuestion.push('Ничья\nВыбор игрока: ');
+      userQuestion.push('Вы выиграли\nВыбор игрока: ');
+      userQuestion.push('Вы проиграли\nВыбор игрока: ');
+      userQuestion.push('\nВыбор компьютера: ');
     }
-    return userQuestion
+    return userQuestion;
   };
 
   const game = (language) => {
@@ -40,21 +39,21 @@
       player: 0,
       computer: 0,
     };
-    language = prompt("If you want play in English enter ENG or EN").toUpperCase()
+    language = prompt(
+      'If you want play in English enter ENG or EN',
+    ).toUpperCase();
     const lang =
       language === 'EN' || language === 'ENG' ? FIGURES_ENG : FIGURES_RUS;
-    const questions = getFigure(lang)
+    const questions = getFigure(lang);
     return function start() {
       // сюда писать логику игры
-      let userChoice = prompt(questions[0]); 
+      let userChoice = prompt(questions[0]);
 
       const computerChoiceNumber = getRandomIntInclusive(0, 2);
       let computerChoice = '';
       if (userChoice === null) {
-        let end = confirm(questions[1]);
-        alert(
-          questions[2] + result.player + questions[3] +result.computer
-        );
+        const end = confirm(questions[1]);
+        alert(questions[2] + result.player + questions[3] + result.computer);
         return end;
       }
       for (let i = 0; i < lang.length; i++) {
@@ -67,30 +66,20 @@
       }
 
       if (computerChoice === userChoice) {
-        
-        alert(
-          questions[4] + userChoice + questions[7] + computerChoice
-
-        );
+        alert(questions[4] + userChoice + questions[7] + computerChoice);
       } else if (
         (computerChoice === lang[0] && userChoice === lang[2]) ||
         (computerChoice === lang[2] && userChoice === lang[1]) ||
         (computerChoice === lang[1] && userChoice === lang[0])
       ) {
-        alert(
-          questions[5] + userChoice + questions[7] + computerChoice
-
-        );
+        alert(questions[5] + userChoice + questions[7] + computerChoice);
         result.player += 1;
       } else {
-        alert(
-          questions[6] + userChoice + questions[7] + computerChoice
-
-        );
+        alert(questions[6] + userChoice + questions[7] + computerChoice);
         result.computer += 1;
       }
       start();
     };
   };
-  window.RPS = game;
+  window.rps = game;
 })();
